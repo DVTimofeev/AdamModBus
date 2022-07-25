88a3e9379e9529235709b05e55e5ee1b2a2279e1
Dobrynya <dobtimofeev@gmail.com>
2022-07-25 13:33:32 +0300

    Valve_2_2 struct for controling one-way valves implemented

--- main.cpp
+++ main.cpp
@@ -1,46 +1,48 @@
 #include <iostream>
-// #include <boost/asio.hpp>
+#include <string>
 #include "modbusTCP.h"
 #include <array>
 
+
+#include <optional>
+#include <sstream>
+
+struct Valve_2_2{
+    Valve_2_2(modbus::ModbusTCP* adam, uint8_t address) : adam_(adam), address_(address){}
+    void open()
+    {
+        adam_->write_DO(address_, true);
+    }
+    void close()
+    {
+        adam_->write_DO(address_, false);
+    }
+    bool is_opened()
+    {
+        return adam_->read_DO(address_).value_or(false);
+    }
+    private:
+        uint8_t address_;
+        modbus::ModbusTCP* adam_;
+};
 
 using boost::asio::ip::tcp;
 
 int main()
 {
     boost::asio::io_service service;
-    modbus::ModbusTCP adam5000(service, "192.168.1.10", 502, "adam5000");
-    adam5000.connect();
-    // int a;
-    // std::cin >> a;
-    // std::cout << std::boolalpha << adam5000.is_connected();
-    std::cout << adam5000.read_AO(48);
-//    tcp::endpoint adam5000(boost::asio::ip::address::from_string("192.168.1.10"), 502);
-//    tcp::socket sock(service);
-//    sock.connect(adam5000);
-    // отправка
-//    uint8_t send[16];
-//    std::array<uint8_t,16> send{1};
-//    send[0] = 0;
-//    send[1] = 0;
-//    send[2] = 0;
-//    send[3] = 0;
-//    send[4] = 0;
-//    send[5] = 6;
-//    send[6] = 1;
-//    send[7] = 3;
-//    send[8] = 0;
-//    send[9] = 48;
-//    send[10] = 0;
-//    send[11] = 1;
-//    auto a = sock.write_some(boost::asio::buffer(send, 12));
+    modbus::ModbusTCP* adam5000 = new modbus::ModbusTCP(service, "192.168.1.10", 502, "adam5000");
+    adam5000->connect();
+    // adam5000->write_DO(49, false);
+    Valve_2_2 valve_49(adam5000, 49);
+    valve_49.open();
+    std::cout << std::boolalpha << "Open : " << valve_49.is_opened() << std::endl;
+    valve_49.open();
+    std::cout << std::boolalpha << "Open : " << valve_49.is_opened() << std::endl;
+    valve_49.close();
+    std::cout << std::boolalpha << "Close : " << valve_49.is_opened() << std::endl;
+    
+    
 
-//    modbus::ModbusTCP modbusTCP(service)
-//    //прием
-//    uint8_t rcv[16];
-//    sock.read_some(boost::asio::buffer(rcv));
-//    uint16_t b = rcv[9];
-//    b = (b << 8) | rcv[10];
-//    std::cout << b;
     return 0;
 }
